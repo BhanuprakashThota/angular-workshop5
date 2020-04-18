0474ab51d789557bcca77557c1706eaa318a157d
Bhanuprakash <thotabhanu03@gmail.com>
2020-04-18 14:32:29 -0500

    Header Change

--- src/app/apifetch/apifetch.component.ts
+++ src/app/apifetch/apifetch.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
+import { HttpClient , HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-apifetch',
@@ -10,15 +11,19 @@ export class APIFetchComponent implements OnInit {
 
 brews: Object;
 
-  constructor(private _http: HttpService) { }
+  //constructor(private _http: HttpService) { }
+  constructor(private http: HttpClient, private headers: HttpHeaders) { }
 
   ngOnInit() {
 
-    this._http.getBeer().subscribe(data => {
-      this.brews = data
-      console.log(this.brews);
-    }
-  );
+    // this._http.getBeer().subscribe(data => {
+    //   this.brews = data
+    //   console.log(this.brews);
+    // }
+  //);
+
+  this.http.get('https://api.openbrewerydb.org/breweries', 
+    {headers : new HttpHeaders().set('Access-Control-Allow-Origin', '*')}).subscribe();
   }
 
 }
