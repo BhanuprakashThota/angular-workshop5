a7a401fff73c375805915b9057503a574361be49
Bhanuprakash <thotabhanu03@gmail.com>
2020-04-18 15:12:54 -0500

    Final Commit

--- src/app/apifetch/apifetch.component.ts
+++ src/app/apifetch/apifetch.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../http.service';
-import { HttpClient , HttpHeaders} from '@angular/common/http';
+
 
 @Component({
   selector: 'app-apifetch',
@@ -9,22 +9,16 @@ import { HttpClient , HttpHeaders} from '@angular/common/http';
 })
 export class APIFetchComponent implements OnInit {
 
-brews: Object;
+  brews: Object;
 
-  //constructor(private _http: HttpService) { }
-  constructor(private http: HttpClient, private headers: HttpHeaders) { }
+  constructor(private _http: HttpService) { }
 
   ngOnInit() {
-
-    // this._http.getBeer().subscribe(data => {
-    //   this.brews = data
-    //   console.log(this.brews);
-    // }
-  //);
-
-  this.http.get('https://api.openbrewerydb.org/breweries', 
-    {headers : new HttpHeaders().set('Access-Control-Allow-Origin', '*')}).subscribe();
+    this._http.getBeer().subscribe(data => {
+      this.brews = data
+      console.log(this.brews);
+    }
+  );
   }
 
 }
- 
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient , HttpHeaders} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor(private http: HttpClient, private headers: HttpHeaders) { }
-  // myMethod() {
-  //   return console.log('Hey, what is up!');
-  // }
+  constructor(private http: HttpClient ) { }
+
   getBeer() {
-   // let header = this.headers.set('Access-Control-Allow-Origin', '*');
-    return this.http.get('https://api.openbrewerydb.org/breweries', 
-    {headers : new HttpHeaders().set('Access-Control-Allow-Origin', '*')});
+
+    return this.http.get('https://api.openbrewerydb.org/breweries');
 }
 }
