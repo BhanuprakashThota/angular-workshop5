55f0703c6c7039072cc1eb5d0e5efb1b87a28d73
Bhanuprakash <thotabhanu03@gmail.com>
2020-04-18 14:25:34 -0500

    Commit Header modification

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient , HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private headers: HttpHeaders) { }
   // myMethod() {
   //   return console.log('Hey, what is up!');
   // }
   getBeer() {
-    return this.http.get('https://api.openbrewerydb.org/breweries');
-  }
+   // let header = this.headers.set('Access-Control-Allow-Origin', '*');
+    return this.http.get('https://api.openbrewerydb.org/breweries', 
+    {headers : new HttpHeaders().set('Access-Control-Allow-Origin', '*')});
+}
 }
